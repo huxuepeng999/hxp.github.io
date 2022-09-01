8de1b437e6188fdd75b29aa8a60533ba5ab11a22
小神 <huxuepeng@gs-robot.com>
2022-09-01 19:48:08 +0800

    feat: 初版

--- src/Foo1/index.tsx
+++ src/Foo1/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Foo } from 'dumi-hxp'
 
 const Foo1 = (props: any) => {
 
     const { title } = props
 
     return (
-        <div>{`Foo1${title}`}</div>
+        <div>{`Foo1${title}`}
+            <Foo title="t1" />
+        </div>
     )
 }
 
