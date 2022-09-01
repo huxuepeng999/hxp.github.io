ac29bcb3bc5e2c2bef67912cbf36423e8b862963
小神 <huxuepeng@gs-robot.com>
2022-09-01 19:37:00 +0800

    feat: 初版

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   base: '/',
-  publicPath: '/page/'
+  publicPath: '/public/',
+  history: {
+    type: 'hash',
+  },
   // more config: https://d.umijs.org/config
 });
